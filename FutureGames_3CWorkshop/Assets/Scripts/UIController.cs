fd92fa515d8f90ad30d86dcf7674d037689935dc
Saheel11 <Saheel96@gmail.com>
2022-09-30 14:29:20 +0200

    Gamecontroller added

--- FutureGames_3CWorkshop/Assets/Scripts/UIController.cs
+++ FutureGames_3CWorkshop/Assets/Scripts/UIController.cs
@@ -8,13 +8,17 @@ using TMPro;
 public class UIController : MonoBehaviour
 {
     public GameObject player;
+    public GameObject gameController;
     public Slider meterSlider;
     public Slider cooldownSlider;
     public TextMeshProUGUI cooldownText;
+
+
+
     
     private void Update()
     {
-        cooldownText.text = player.GetComponent<PlayerStats>().dashCooldown.ToString();
+        cooldownText.text = gameController.GetComponent<GameController>().timer.ToString("f0");
         meterSlider.value = player.GetComponent<PlayerStats>().meter;
         cooldownSlider.value = player.GetComponent<PlayerStats>().dashCooldown;
     }
