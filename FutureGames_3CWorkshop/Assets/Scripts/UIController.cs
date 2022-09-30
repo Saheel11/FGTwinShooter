bdf1b9c4cc64134f521e2db9873dad632f536d1f
Saheel11 <Saheel96@gmail.com>
2022-09-30 13:45:17 +0200

    added cooldown number text if we want that

--- FutureGames_3CWorkshop/Assets/Scripts/UIController.cs
+++ FutureGames_3CWorkshop/Assets/Scripts/UIController.cs
@@ -3,21 +3,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class UIController : MonoBehaviour
 {
     public GameObject player;
     public Slider meterSlider;
     public Slider cooldownSlider;
-
-    private void Start()
-    {
-
-    }
-
-    // Update is called once per frame
+    public TextMeshProUGUI cooldownText;
+    
     private void Update()
     {
+        cooldownText.text = player.GetComponent<PlayerStats>().dashCooldown.ToString();
         meterSlider.value = player.GetComponent<PlayerStats>().meter;
         cooldownSlider.value = player.GetComponent<PlayerStats>().dashCooldown;
     }
