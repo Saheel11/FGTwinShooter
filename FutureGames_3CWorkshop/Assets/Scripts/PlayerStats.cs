6ad7c689fa1bc15e17f5415c3aca1ae4a5f6c0ac
Saheel11 <Saheel96@gmail.com>
2022-09-30 12:32:13 +0200

    Added health and dash cooldown
    added testing inputs as well

--- FutureGames_3CWorkshop/Assets/Scripts/PlayerStats.cs
+++ FutureGames_3CWorkshop/Assets/Scripts/PlayerStats.cs
@@ -4,15 +4,60 @@ using UnityEngine;
 
 public class PlayerStats : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public int meter;
+    
+    public int increaseMeter; // testing move to player dash
+    public int decreaseMeter; // testing move to enemy projectile
+    
+    
+    public float dashCooldown;
+    public float dashStartCooldown;
+    public bool canDash;
+    
+    
     void Update()
     {
-        
+        //Testing with manual inputs
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            IncreaseMeter();
+        }
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            DecreaseMeter();
+        }
+        if (Input.GetKeyDown(KeyCode.Z) && canDash)
+        {
+            StartDashCooldown();
+        }
+         
+        if (dashCooldown > 0)
+        {
+            dashCooldown -= Time.deltaTime;
+        }
+        if (dashCooldown <= 0)
+        {
+            canDash = true;
+        }
+        else
+        {
+            canDash = false;
+        }
+    }
+
+
+    public void IncreaseMeter()
+    {
+        meter += increaseMeter;
+    }
+    
+    public void DecreaseMeter()
+    {
+        meter -= decreaseMeter;
+    }
+
+    public void StartDashCooldown()
+    {
+        dashCooldown = dashStartCooldown;
     }
 }
