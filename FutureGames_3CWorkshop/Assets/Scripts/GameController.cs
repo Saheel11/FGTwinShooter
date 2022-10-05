f43f5e99aaaba8da0e3703b05f1e9165b92bf7bb
erik <erik.silfversparre@edu.futuregames.nu>
2022-10-05 17:12:41 +0200

    First attempt to create gamestates

--- FutureGames_3CWorkshop/Assets/Scripts/GameController.cs
+++ FutureGames_3CWorkshop/Assets/Scripts/GameController.cs
@@ -8,7 +8,10 @@ public class GameController : MonoBehaviour
     public float timer;
     public float startTime = 300f;
     public float endTime;
-    
+
+    public GameState State;
+
+    public static event Action<GameState> OnGameStateChanged;
     
     //public int min = Mathf.FloorToInt(timer / 60); testing if we can turn them into minutes and seconds
     //public int sec = Mathf.FloorToInt(timer % 60);
@@ -18,6 +21,7 @@ public class GameController : MonoBehaviour
     private void Start()
     {
         timer = startTime;
+        ChangeGameState(GameState.Startscreen);
     }
 
     private void FixedUpdate()
@@ -30,4 +34,40 @@ public class GameController : MonoBehaviour
         }
         
     }
+    public enum GameState
+    {
+        Startscreen, 
+        Play,
+        Win,
+        Lose
+    }
+    public void ChangeGameState(GameState newState)
+    {
+        State = newState;
+
+        switch (newState)
+        {
+            case GameState.Startscreen:
+                break;
+            case GameState.Play:
+                HandlePlay();
+                break;
+            case GameState.Win:
+                break;
+            case GameState.Lose:
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(newState), newState, null);
+        }
+        OnGameStateChanged(newState);
+    }
+
+    private void HandlePlay()
+    {
+        if (meter >= 100) ChangeGameState(GameState.Win);
+        else if (timer <= 0) ChangeGameState(GameState.Lose);
+        else ChangeGameState(GameState.Play);
+
+    }
+
 }
