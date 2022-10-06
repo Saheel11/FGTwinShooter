ed43f6cf997c80f94cbd5160288156715718bb2e
erik <erik.silfversparre@edu.futuregames.nu>
2022-10-06 12:04:57 +0200

    Latest changes

--- FutureGames_3CWorkshop/Assets/Scripts/GameController.cs
+++ FutureGames_3CWorkshop/Assets/Scripts/GameController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
@@ -9,19 +10,22 @@ public class GameController : MonoBehaviour
     public float startTime = 300f;
     public float endTime;
 
-    public GameState State;
+    public PlayerStats playerStats;
+    
+
+    /*public GameState State; THIS IS FOR GAMESTATES
 
     public static event Action<GameState> OnGameStateChanged;
     
     //public int min = Mathf.FloorToInt(timer / 60); testing if we can turn them into minutes and seconds
     //public int sec = Mathf.FloorToInt(timer % 60);
-    
+    */
 
 
     private void Start()
     {
         timer = startTime;
-        ChangeGameState(GameState.Startscreen);
+        //ChangeGameState(GameState.Startscreen); THIS IS FOR GAMESTATES
     }
 
     private void FixedUpdate()
@@ -33,14 +37,41 @@ public class GameController : MonoBehaviour
             timer = endTime;
         }
         
+        if (timer <= 0)
+        {
+            GameLose();
+        }
+        if (playerStats.meter >= 100)
+        {
+            GameWin();
+        }
+        /*if (meter <= 0)
+        {
+            GameLose();
+        }*/
     }
-    public enum GameState
-    {
-        Startscreen, 
-        Play,
-        Win,
-        Lose
+
+    public void GameLose()
+    {              
+       SceneManager.LoadScene("LoseScene");       
     }
+   
+    public void GameWin()
+    {             
+       SceneManager.LoadScene("WinScene");       
+    }
+
+   
+
+    /* public enum GameState THIS IS FOR GAMESTATES
+     {
+         Startscreen, 
+         Play,
+         Win,
+         Lose
+     }
+    
+
     public void ChangeGameState(GameState newState)
     {
         State = newState;
@@ -48,9 +79,10 @@ public class GameController : MonoBehaviour
         switch (newState)
         {
             case GameState.Startscreen:
+                HandleStartscreen();
                 break;
             case GameState.Play:
-                HandlePlay();
+                //HandlePlay();
                 break;
             case GameState.Win:
                 break;
@@ -60,6 +92,7 @@ public class GameController : MonoBehaviour
                 throw new ArgumentOutOfRangeException(nameof(newState), newState, null);
         }
         OnGameStateChanged(newState);
+        
     }
 
     private void HandlePlay()
@@ -69,5 +102,13 @@ public class GameController : MonoBehaviour
         else ChangeGameState(GameState.Play);
 
     }
+    
+
+    private void HandleStartscreen()
+    {
+
+    }
+    */
+
 
 }
