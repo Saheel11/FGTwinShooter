cd61b3f4b0f39a3c27365f4775c9c7e296747c3b
Saheel11 <Saheel96@gmail.com>
2022-10-06 11:43:43 +0200

    limit amount of enemies spawning

--- FutureGames_3CWorkshop/Assets/Scripts/EnemyManager.cs
+++ FutureGames_3CWorkshop/Assets/Scripts/EnemyManager.cs
@@ -8,13 +8,16 @@ public class EnemyManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
     public float enemySpawnTimer;
-    public int amountOfEnemiesSpawned; 
     public float enemySpawnTimeCooldown = 5; 
+    public int maxAmountOfEnemies;
+    public int amountOfEnemiesInLevel;
+
 
     [SerializeField] Transform[] possibleSpawnPositions;
     bool[] usedSpawnPositions;
+    
     public PlayerStats playerStats;
-    private int[] enemyAmount;
+
 
 
     int checkedSpawnPositions;
@@ -32,37 +35,43 @@ public class EnemyManager : MonoBehaviour
         enemySpawnTimer += Time.deltaTime; //Starting timer for enemy to spawn
         if (enemySpawnTimer >= enemySpawnTimeCooldown)
         {
-            Debug.Log("Spawning 1 enemy");
             RandomSpawn();
             enemySpawnTimer = 0; //resets timer to 0 every time an enemy spawn
         }
         
-        // Decrease time between enemies spawning depending on the meter
-        if (playerStats.meter >= 80) 
+        // Decrease time between enemies spawning & limit amount of enemies spawning depending on the meter 
+        if (playerStats.meter > 80) 
         {
             enemySpawnTimeCooldown = 0.5f;
+            maxAmountOfEnemies = 50;
         }
-        else if (playerStats.meter >= 60) 
+        else if (playerStats.meter > 60) 
         {
             enemySpawnTimeCooldown = 1;
+            maxAmountOfEnemies = 40;
         }
-        else if (playerStats.meter >= 40) 
+        else if (playerStats.meter > 40) 
         {
             enemySpawnTimeCooldown = 1.25f;
+            maxAmountOfEnemies = 30;
         }
-        else if (playerStats.meter >= 20) 
+        else if (playerStats.meter > 20) 
         {
             enemySpawnTimeCooldown = 1.5f;
+            maxAmountOfEnemies = 20;
         }
         else if (playerStats.meter < 20)
         {
             enemySpawnTimeCooldown = 2;
+            maxAmountOfEnemies = 10;
         }
     }
     
     public void RandomSpawn()
     {
-        int spawnIndex = Random.Range(0, possibleSpawnPositions.Length);
+        if (amountOfEnemiesInLevel <= maxAmountOfEnemies)
+        {
+            int spawnIndex = Random.Range(0, possibleSpawnPositions.Length);
             
             if (spawnIndex == checkedSpawnPositions)
             {
@@ -78,6 +87,10 @@ public class EnemyManager : MonoBehaviour
 
             GameObject newEnemy = Instantiate(enemyPrefab, possibleSpawnPositions[spawnIndex].position, possibleSpawnPositions[spawnIndex].rotation);
             checkedSpawnPositions = spawnIndex;
+            amountOfEnemiesInLevel++;
+            Debug.Log("Spawning 1 enemy");
+
+        }
 
     }
 }
